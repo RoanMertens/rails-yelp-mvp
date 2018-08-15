65e03932e852984ac1256cc4952a7a95742662ad
Roan Mertens <roanmertens91@gmail.com>
2018-08-15 19:46:14 +0100

    one more rake

---
+++ app/models/review.rb
@@ -0,0 +1,12 @@
+class Review < ApplicationRecord
+  belongs_to :restaurant
+  validates :content, presence: true
+  validates :rating, numericality: true, presence: true, inclusion: { in: [0, 1, 2, 3, 4, 5] }
+end
+
+
+# class Review < ApplicationRecord
+#   belongs_to :restaurant
+#   validates :content, presence: true
+#   validates :rating, numericality: true, presence: true, inclusion: { in: (0..5) }
+# end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,53 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+restaurants_attributes = [
+  {
+    name:         'Dishoom',
+    address:      '7 Boundary St, London E2 7JE',
+    description:  'Buzzy destination for Indian street food in Bombay-style vintage decor.',
+    rating:        5,
+    category: "chinese",
+    phone_number: "8475908275"
+  },
+  {
+    name:         'Pizza East',
+    address:      '56A Shoreditch High St, London E1 6PQ',
+    description:  'Pizzeria with industrial looks, serving rustic pizza and antipasti.',
+    rating:        1,
+    category: "chinese",
+    phone_number: "8475908275"
+  },
+  {
+    name:         'Pizza Pilgrims',
+    address:      'London E1 6sfssdfffsQ',
+    description:  'Pizzeria with new pizza.',
+    rating:        5,
+    category: "chinese",
+    phone_number: "8475908275"
+  },
+  {
+    name:         'walhalla',
+    address:      'sdfdsfsdddddddddddddddddddddddondon E1 6PQ',
+    description:  'sumthing with food jummy',
+    rating:        3,
+    category: "chinese",
+    phone_number: "8475908275"
+  },
+  {
+    name:         'boxpark',
+    address:      'shoreditch boxpark',
+    description:  'lots of different kind of foods',
+    rating:        4,
+    category: "chinese",
+    phone_number: "8475908275"
+  }
+]
+Restaurant.create!(restaurants_attributes)
+puts 'Finished!'
